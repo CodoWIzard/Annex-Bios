b048f21c45e63f220e318115772818f19fdb7fdd
Jayen <jayden.budike@gmail.com>
2024-09-05 11:23:33 +0200

    Folder structure change

--- core/header.php
+++ core/header.php
@@ -29,36 +29,5 @@
             <button>Bestel je Tickets</button>
         </div>
     </div>
-    <div class="header-image">
-        <img alt="Header image" src="/assets/img/header_afbeelding.jpg" width="100%">
-    </div>
-    <footer class="footer">
-        <div class="logo-section">
-            <img alt="AnnexBios logo" src="/assets/img/logo_wit.png" width="150" height="50">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean convallis ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis,</p>
-            <button>LEES MEER</button>
-        </div>
-        <div class="nav-section">
-            <h3>NAVIGEER</h3>
-            <ul>
-                <li>Winkel bij</li>
-                <li>Veelgestelde vragen</li>
-                <li>Wegwijzers</li>
-                <li>Contact</li>
-            </ul>
-        </div>
-        <div class="social-section">
-            <h3>VOLG ONS</h3>
-            <i class="fab fa-facebook-f"></i>
-            <i class="fab fa-twitter"></i>
-            <i class="fab fa-instagram"></i>
-        </div>
-        <div class="film-strip"></div>
-    </footer>
-    <div class="bottom-bar">
-        <a href="#">Voorwaarden</a> |
-        <a href="#">Privacy beleid</a> |
-        <a href="#">Cookie disclaimer</a>
-    </div>
 </body>
 </html>
---
+++ core/index.php
@@ -0,0 +1,5 @@
+<?php include "header.php";?>
+<div class="header-image">
+        <img alt="Header image" src="/assets/img/header_afbeelding.jpg" width="100%">
+    </div>
+<?php include "footer.php";?>
