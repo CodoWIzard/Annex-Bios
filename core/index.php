bbbd2ddedfc136d0ecc78efaadf2d1079ec41c95
Jayen <jayden.budike@gmail.com>
2024-09-05 16:11:05 +0200

    Paar changes

--- core/index.php
+++ core/index.php
@@ -1,5 +1,14 @@
 <?php include "header.php";?>
-<div class="header-image">
+<div class="main">
+
+    <div class="header-image">
         <img alt="Header image" src="/assets/img/header_afbeelding.jpg" width="100%">
     </div>
+
+    <div class="welcome-section">
+        <h1>Welcome to AnnexBios</h1>
+    </div>
+
+</div>
+
 <?php include "footer.php";?>
